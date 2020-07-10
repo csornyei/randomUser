ade25a25e2ef229e33b1c0a602e310137548643f
Mate Csornyei <mate@csornyei.com>
2020-07-10 21:53:43 +0200

    UserList basic

--- src/UserList/UserList.tsx
+++ src/UserList/UserList.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import UserListItem from './UserListItem';
+import styles from './UserList.module.scss'
 
 const UserList : React.FC = props => {
 
     const users = [1, 2, 3, 4, 5, 6];
 
-    const listItems = users.map((user) => {
-        return <UserListItem />
+    const listItems = users.map((user, idx) => {
+        return <UserListItem index={idx} key={idx}/>
     })
 
     return (
-        <>
+        <div className={styles.container}>
             {listItems}
-        </>
+        </div>
     );
 }
 
--- src/UserList/UserListItem.tsx
+++ src/UserList/UserListItem.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from './UserListItem.module.scss';
 
-const UserListItem : React.FC = props => {
+const UserListItem = (props: {index: number}) => {
+
+
+    const classes = props.index % 2 === 0 ? [styles.item, styles.itemEven] : [styles.item, styles.itemOdd];
+
     return (
-        <>
+        <div className={classes.join(' ')}>
             User List Item
-        </>
+        </div>
     );
 }
 
